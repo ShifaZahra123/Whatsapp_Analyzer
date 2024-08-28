51f47e0c20af084a1b5493a2042014c060d50971
Shifa Zahra <140159309+ShifaZahra123@users.noreply.github.com>
2024-08-28 14:27:19 +0500

    app.py

--- src/app.py
+++ src/app.py
@@ -35,8 +35,9 @@ if uploaded_file is not None:
             # Ensure all values in the 'users' column are strings
   #  df['users'] = df['users'].astype(str)
         # preprocessing data, making data frame
+        
+        df = preprocessor.preprocess(data)
         df['users'] = df['users'].astype(str)
-        # df = preprocessor.preprocess(data)
 
         # Fetch unique user names and arrange them to show in the select box
         user_list = df['users'].unique().tolist()
